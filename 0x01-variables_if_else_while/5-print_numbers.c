d8128a30bfaf80758d854fff3ff8a6895a8596fe
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-09 03:52:43 -0700

    Create 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n < '10'; n++)
+  {
+		printf("%d\n", n);
+	}
+  return (0);
+}

f839ba51bf0be69ca5b9ef96be56c848ea28caca
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-09 03:11:39 -0700

    Create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char D;
+
+	D = 'a';
+  while (D <= 'z');
+  {
+		putchar(low);
+		putchar('\n');
+  D++
+    }
+
+	return (0);
+}

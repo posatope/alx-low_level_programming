c14e5c6971fdcad4d2f59432f7adfe59bfc2c124
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-09 02:30:58 -0700

    Create 1-last_digit.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+  int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+  /*strlen(n) gives total length of string . string index starts from 0 
+	last index of character is strlen(c)-1*/ 
+  int c;
+  
+	c = strlen(n)-1;
+  
+  if (n[c] > 5)
+  printf ("Last digit of %d is %d and is greater than 5\n", n, n[c]);
+  else if (n[c] == 0)
+    printf ("Last digit of %d is %d and is 0\n", n, n[c]);
+  else
+    printf ("Last digit of %d is %d and is less than 6 and not 0\n", n, n[c]);
+	return (0);
+}

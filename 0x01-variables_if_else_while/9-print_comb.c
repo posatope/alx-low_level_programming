00aea01d526316b1193923faee0d39966665363c
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-09 04:48:42 -0700

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,10 +13,11 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n <9; n++)
+	for (n = 0; n <=9; n++)
 	{
 		putchar(n);
-		putchar(',');
+		if (n < 9)
+			putchar(',');
 		putchar(' ');
 	}
 	putchar('\n');

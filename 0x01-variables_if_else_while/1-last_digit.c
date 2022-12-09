0aea686e5936888a0d881bd3049864728f56a32e
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-09 02:53:27 -0700

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,28 +9,28 @@
  * Return: Always 0 (Success)
  */
 /**
- * strlen(n) gives total length of string . string index starts from 0 
+ * strlen(n) gives total length of string . string index starts from 0
  * last index of character is strlen(c)-1
  */
 
 int main(void)
 {
-  int n, c;
+	int n, c;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
   	c = n % 10;
-  
-  if (c > 5)
-  {
-	  printf("Last digit of %d is %d and is greater than 5\n", n, c);
-  }
-  else if (c == 0)
-  {
-	  printf("Last digit of %d is %d and is 0\n", n, c);
-  }
-  else
-  {
-	  printf("Last digit of %d is %d and is less than 6 and not 0\n", n, c);
-  }
+	
+	if (c > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, c);
+	}
+	else if (c == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, c);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, c);
+	}
 	return (0);
 }

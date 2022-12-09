ee17de48ada67fd475a2f421c3be4293e858ccf0
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-09 03:32:23 -0700

    Create 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char D, C;
+
+	for (D = 'a', C = 'A'; D <= 'z', C <= 'Z'; D++, C++)
+	{
+		putchar(D);
+    putchar(C)
+
+	}
+	putchar('\n');
+	return (0);
+}

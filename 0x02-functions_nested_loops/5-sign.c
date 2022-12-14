66d1cf2dfe0bb5aebee18042e2500fd1e2938b8e
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-14 09:57:29 -0700

    Create 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include <ctype.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_sign - check if int c is a alphabet.
+ * @n: is the value being checked
+ * Returns 1 and prints + if n is greater than zero, Returns 0 and prints 0 if n is zero, Returns -1 and prints - if n is less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0 || n > 1)
+  {
+  return (+);
+  }
+	else if (n < 0 || n < -1)
+  {
+  return (-);
+  }
+  else
+  return (0);
+}

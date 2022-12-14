66edcd7bb9dc3d0941fce2b4dc27549778f30e8f
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-14 09:22:44 -0700

    Create 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include <ctype.h>
+#include "main.h"
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+  int c;
+  
+  printf("Return value when %c is passed to islower(): %d", c, islower(c));
+}

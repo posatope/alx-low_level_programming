c339756f6b9435de0dd18b5cf6bcda3400a3ae12
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-14 12:55:00 -0700

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,19 +12,19 @@
  */
 void jack_bauer(void)
 {
-  int y;
-  int x;
-    
-  for (x = 0; x < 24; x++)
-  {
-    for (y = 0; y < 60; y++)
-    {
-      _putchar((x / 10) + '0');
-      _putchar((x % 10) + '0');
-      _putchar(':');
-      _putchar((y / 10) + '0');
-      _putchar((y % 10) + '0');
-      _putchar('\n');
-    }
-  }
+	int y;
+	int x;
+
+	for (x = 0; x < 24; x++)
+        {
+		for (y = 0; y < 60; y++)
+		{
+			_putchar((x / 10) + '0');
+			_putchar((x % 10) + '0');
+			_putchar(':');
+			_putchar((y / 10) + '0');
+			_putchar((y % 10) + '0');
+			_putchar('\n');
+		}
+	}
 }

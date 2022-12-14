35a5919c31b9ca51a8f59f9d10bb1db74b785fc7
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-14 03:58:42 -0700

    Create 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * print_alphabet_x10 -  prints the alphabet, in lowercase
+ * Description: followed by a new line.
+ * Return: 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+  print_alphabet(i);
+  print_alphabet(i);
+  print_alphabet(i);
+  print_alphabet(i);
+  print_alphabet(i);
+  print_alphabet(i);
+  print_alphabet(i);
+  print_alphabet(i);
+  print_alphabet(i);
+  print_alphabet(i);
+  _putchar('\n')
+}

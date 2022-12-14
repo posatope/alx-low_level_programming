82390c2ed91878663a39792c5ccb1cabc9ff0888
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-14 03:23:39 -0700

    Create 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * main -  prints the alphabet, in lowercase
+ * Description: It prints the word Holberton, followed by a new line.
+ * Return: 0.
+ */

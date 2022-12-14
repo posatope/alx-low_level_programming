4c51b56d33e532d9a74032b06c97f7592eae8613
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-14 15:37:39 -0700

    Update positive_or_negative.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -13,17 +13,6 @@ int positive_or_negative(void)
 	int n;
 
 	n = 0;
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else
-	{
-		printf("%d is negative\n", n);
-	}
+	printf("%d is zero\n", n);
 	return (0);
 }

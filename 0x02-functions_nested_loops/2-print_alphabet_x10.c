61893c82a9c8fbdbdc4281953a231486d66d3abc
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-14 04:09:42 -0700

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,8 +11,8 @@ void print_alphabet_x10(void)
 	char i;
 	int a;
 	
-	for (i = 'a'; i <= 'z'; i++)
-		for (a = 0; a < 10; a++)
+	for (a = 0; a < 10; a++)
+		for (i = 'a'; i <= 'z'; i++)
 			_putchar(i);
 	_putchar('\n');
 }

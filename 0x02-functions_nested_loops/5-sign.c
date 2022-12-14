ebb5598e4ddbbd79c27d3476a7520aa0cd791a9d
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-14 10:05:36 -0700

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,16 +9,16 @@
 int print_sign(int n)
 {
 	if (n > 0 || n > 1)
-  {
+	{
 		_putchar ('+');
 		return (1);
-  }
+	}
 	else if (n < 0 || n < -1)
-  {
+	{
 		_putchar ('-');
 		return (-1);
-  }
-  else
-	_putchar (0);
+	}
+	else
+		_putchar (0);
 	return (0);
 }

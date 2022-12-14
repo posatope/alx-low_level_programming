c06e5f852b6fe30e79ad59f217346e797d9964ce
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-14 13:03:25 -0700

    Create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+/**
+ * print_last_digit - check the code
+ * @c: is the digit whose last digit we are looking for
+ * Return: last digit of a number
+ */
+int print_last_digit(int c)
+{
+	int y;
+	if (c == INT_MIN)
+	{
+	y = 8;
+	}
+	else
+	{
+	y = abs(c);
+	}
+
+	_putchar(y % 10 + '0');
+	return (y % 10);
+}

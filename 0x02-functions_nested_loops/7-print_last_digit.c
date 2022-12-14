17501862a1419e286ace825e3fff061a90413657
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-14 11:33:32 -0700

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,7 +6,7 @@
 /**
  * print_last_digit - check the code
  * @c: is the digit whose last digit we are looking for
- * Return: last digit of a number.
+ * Return: last digit of a number
  */
 int print_last_digit(int c)
 {

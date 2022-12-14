bf1942b08a8cdd9c0db58d97b0d866d38b2723e4
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-14 13:55:55 -0700

    gdhdg

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+  * main - print the first 50 fibonacci numbers.
+  * Return: Nothing.
+  */
+int main(void)
+{
+	unsigned long count, i, j, k;
+
+	i = 1;
+	j = 2;
+	for (count = 0; count < 98; count++)
+	{
+		k = i + j;
+		i = j;
+		j = k;
+		printf("%lu", k);
+		if (count == 49)
+			putchar('\n');
+		else
+		{
+			printf(", ");
+		}
+	}
+	return (0)

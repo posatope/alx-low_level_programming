c16a115c21de320b6357f199e7102a4ab124c8e3
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-14 10:08:27 -0700

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,8 @@
 /**
  * print_sign - check if int c is a alphabet.
  * @n: is the value being checked
- * Returns 1 and prints + if n is greater than zero, Returns 0 and prints 0 if n is zero, Returns -1 and prints - if n is less than zero
+ * Returns 1 and prints + if n is greater than zero, Returns 0 and prints 0 
+ * if n is zero, Returns -1 and prints - if n is less than zero
  */
 int print_sign(int n)
 {

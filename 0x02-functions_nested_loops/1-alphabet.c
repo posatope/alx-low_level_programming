84672d4dc90d2834412a2358de24994281f9864c
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-14 03:40:24 -0700

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,3 +6,13 @@
  * Description: It prints the word Holberton, followed by a new line.
  * Return: 0.
  */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z' ; i++)
+		print_alphabet(i);
+	_putchar('\n');
+
+	return (0);

518678ef487cb214f44e1947031ba75041fcab79
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-14 03:36:05 -0700

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
 #include <unistd.h>
 #include "main.h"
 /**
- * main - check the code
+ * main - write putchar and add one line
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-  char c[8] = "_putchar";
-  
-  int i;
-  
-  for (i=0; i<8; i++)
-    _putchar(c[i]);
-  
-  _putchar('\n');
-  return 0;
+	char c[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+		_putchar(c[i]);
+	_putchar('\n');
+
+	return (0);
 }

2a762601ee77a2e68fbef9178f4ae4f5b11cd1c9
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-14 10:28:47 -0700

    Create 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include <ctype.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * int _abs - computes the absolute value of an integer
+ * @int: is the value being checked
+ * Return: 1 if it is a lowercase letter, 0 if it is not a lowercase letter.
+ */
+int _abs(int)
+{
+	return (abs(int);
+}

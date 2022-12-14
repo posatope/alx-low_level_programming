5ffcbc8a18323828b9a93ed5d367a691819b1feb
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-14 11:47:56 -0700

    Create 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <time.h>
+
+/**
+ * jack_bauer - check the code
+ * y is the digit whose last digit we are looking for
+ * Return: last digit of a number
+ */
+void jack_bauer(void)
+{
+  time_t y
+    if (y = 00:00; y <= 23:59: y++)
+      return (y);
+}

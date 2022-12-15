f2c17b540cdad1da44fd0e88bd6cfaba6fd33155
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-15 16:41:46 -0700

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,10 +10,10 @@ void more_numbers(void)
 {
 	int i;
 	int j;
-	
-	for(i = 1; i <= 10; i++) 
+
+	for (i = 1; i <= 10; i++)
 	{
-		for (j=0; j <= 14; j++)
+		for (j = 0; j <= 14; j++)
 		{
 			if (j > 9)
 				_putchar(j / 10 + '0');

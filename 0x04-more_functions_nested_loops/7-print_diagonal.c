33afec06eb0a37a9434716b87aed04df96df4a00
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-15 03:16:12 -0700

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,3 +6,21 @@
  * Return: Always 0.
  */
  void print_diagonal(int n)
+{
+	int a, b;
+
+	if (n <= 0)
+	{
+	    putchar('\n');
+	}
+	else 
+	for(a = 1; a <= n; a++) 
+	{
+		for (b=1; b < a; b++)
+		{
+				putchar(' ');
+		}
+	putchar('\\');
+	putchar('\n');
+	}
+}

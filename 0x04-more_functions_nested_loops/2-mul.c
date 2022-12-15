442588a78b2018d5b40f30320d985c1bb361b9b4
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-15 13:13:38 -0700

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,12 @@
 #include "main.h"                                                                                                                                         
 #include <stdio.h>                                                                                                                                        
-/**                                                                                                                                                       
- *  * mul - check the code                                                                                                                                
- *   * @a: number 1                                                                                                                                       
- *    * @b: number 2                                                                                                                                      
- *     * return: multiplier.                                                                                                                              
- *      */                                                                                                                                                
-int mul(int a, int b)                                                                                                                                     
+/**
+ * mul - function that prints the numbers, from 0 to 9, followed by a new line.
+ * @a: the int for the paramaters of my function
+ * @b: the int for the paramaters of my function
+ * Return: 0
+ */
+int mul(int a, int b)
 {
 	return (a * b);
 }

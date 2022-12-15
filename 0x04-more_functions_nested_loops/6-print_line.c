9bf26f9c29e8468964310539c895e964f7c6e9c1
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-15 02:33:56 -0700

    Create 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * print_line - check the code
+ * @n: the number processed
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	int a;
+	
+	for(a = 1; a <= n; a++)
+	{
+		if (a <= 0)
+		{
+		    _putchar('\n');
+		}
+		else
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}

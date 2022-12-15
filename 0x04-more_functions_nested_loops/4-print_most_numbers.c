8c759c37af2852d963e4e7b31ef3bad81fd9cf27
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-15 03:35:23 -0700

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,15 +10,13 @@ void print_most_numbers(void)
     int a;
 	
 	for (a = 0; a <= 9; a++)
-	{
-	    if (a ==2 || a==4)
+	if (a ==2 || a==4)
 	{
 	    continue;
 	}
 	else
 	{
 	    _putchar(a + '0');
-	}
 	}
 	_putchar('\n');
 }

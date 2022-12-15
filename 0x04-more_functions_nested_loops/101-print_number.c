ed9450f785b1d32694e28a563292ee6243faa58d
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-15 14:04:46 -0700

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,33 +6,11 @@
 
 void print_number(int n)
 {
-	long len, res, i, temp, expo;
-
-	res = n;
-	expo = len =  1;
-/*Check negatives*/
-	if (res < 0)
+    int a;
+	
+	for (a = 0; a <= 9; a++)
 	{
-		res *= -1;
-		_putchar('-');
+	    _putchar(a + '0');
 	}
-
-/**/
-	temp = res;
-	while (temp >= 10)
-	{
-		len++;
-		temp /= 10;
-	}
-
-/*Create Exponent*/
-	for (i = 1; i < len; i++)
-		expo *= 10;
-/*Main */
-	while (expo > 1)
-	{
-		_putchar((res / expo) % 10 + '0');
-		expo /= 10;
-	}
-	_putchar(res % 10 + '0');
+	_putchar('\n');
 }

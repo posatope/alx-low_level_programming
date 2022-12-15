ff5df664ece3b1410690a83687662fca18c00991
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-15 02:10:29 -0700

    Create 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * more_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+	int i, j;
+	for(i = 1; i <= 10; i++) 
+	{
+		for (j=0; j <= 14; j++)
+		{
+			printf("%d", j);
+		}
+		_putchar('\n');
+		}
+}

efeb75a7ac45cf2bc4ad4361fb3869cc1591a4e6
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-15 18:28:01 -0700

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,23 +11,23 @@
 
 int largest_number(int a, int b, int c)
 {
-    int large;
+int large;
 
-    if (a > b && a > c)
-    {
-        large = a;
-    }
-    else if (b > a && b > c)
-    {
-        large = b;
-    }
-    else if (c > a && c > b)
-    {
-        large = c;
-    }
-    else
-    {
-        printf("\n");
-    }
-    return (large);
+	if (a > b && a > c)
+	{
+		large = a;
+	}
+	else if (b > a && b > c)
+	{
+		large = b;
+	}
+	else if (c > a && c > b)
+	{
+		large = c;
+	}
+	else
+	{
+		printf("\n");
+	}
+	return (large);
 }

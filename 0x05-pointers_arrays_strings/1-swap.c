aff2635eb2d9f1be4fced1c5891464ef9a85bc5c
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-19 13:49:57 -0700

    Create 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+	int d;
+	
+	c = a;
+	d = b;
+	a = d;
+	b = c;
+}

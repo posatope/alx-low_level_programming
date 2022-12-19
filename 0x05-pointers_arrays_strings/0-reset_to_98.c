506b836c709243084d1f033723c3e1d518736366
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-19 13:45:10 -0700

    Create 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+#include <unistd.h>
+/**
+ * _atoi - prints the alphabet 10 times
+ * @*s: prints each on separate lines
+ * Return: 0.
+ */
+void reset_to_98(int *n)
+{
+	n = 98
+}

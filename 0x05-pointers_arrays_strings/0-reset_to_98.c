1098ed00a58a0408e35d282b0fbec3028497a18e
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-19 13:54:44 -0700

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -6,7 +6,7 @@
 #include <unistd.h>
 /**
  * reset_to_98 - prints the alphabet 10 times
- * @*n: prints each on separate lines
+ * @n: prints each on separate lines
  * Return: 0.
  */
 void reset_to_98(int *n)

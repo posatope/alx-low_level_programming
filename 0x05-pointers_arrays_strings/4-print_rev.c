465a4853ba5de2a22c0809ecc2d8fad5bf4de605
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-19 15:18:50 -0700

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,7 +13,7 @@ void print_rev(char *s)
 
 	if (n == 0)
 	{
-		break;
+		_putchar('\n');
 	}
 	else
 	{
@@ -21,6 +21,6 @@ void print_rev(char *s)
 		{
 			_putchar(s[n - i]);
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }

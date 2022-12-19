4553e59740be10e5066e96f87aacd9d130409cfe
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-19 14:47:35 -0700

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,6 +8,12 @@
  */
 void print_rev(char *s)
 {
-	strrev(s);
-	return (*s);
+	int n = strlen(*s);
+
+  for (int i = 0; i < n / 2; i++)
+  {
+    char ch = s[i];
+    s[i] = s[n - i - 1];
+    s[n - i - 1] = ch;
+  }
 }

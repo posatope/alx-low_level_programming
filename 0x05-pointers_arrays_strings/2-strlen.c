8c3ecb7b61da5098919523014deebcc23760f88b
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-19 13:59:17 -0700

    Create 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - check the code
+ * @s: string
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int z;
+	
+	z = strlen(s);
+	_putchar(z + '0')
+}

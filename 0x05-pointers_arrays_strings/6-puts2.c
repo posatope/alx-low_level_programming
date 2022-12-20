85ea4bfb40452dbf5d88e976afaf359185b264c3
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-19 18:13:18 -0700

    Create 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * puts2 - check the code
+ * @str: character
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+	int c;
+
+	for (c =0; str[c] != 0; c++)
+	{
+		while c % 2 = 0
+			_putchar(s[c]);
+			}
+	_putchar('\n');
+}

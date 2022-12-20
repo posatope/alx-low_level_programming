1168029f0d2d365590b92dfc533f36e47487ec82
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-19 19:52:40 -0700

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,9 +12,13 @@ void print_array(int *a, int n)
 	int i;
 
 	i = 0;
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n -1; i++)
 	{
 		printf("%d, ", a[i]);
+	}
+	for (i > n - 2 && i < n)
+	{
+		printf("%d ", a[i]);
 	}
 	_putchar('\n');
 }

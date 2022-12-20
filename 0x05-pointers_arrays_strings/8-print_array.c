48b6065a5c93c28704fb6041736bffbcdd493961
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-19 19:10:49 -0700

    Create 8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcpy - check the code
+ * @dest: character
+ * @src: second char
+ * Return: Always 0.
+ */
+void print_array(int *a, int n)
+{
+	int b;
+	
+	b = a[n];
+	for (n = 0;; n++)
+		printf("%d\," a[n]);
+	_putchar('\n');		
+}

6c0beb7dfb280a0eb1612490a38c8eb5dcad618d
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-19 18:40:04 -0700

    Create 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcpy - check the code
+ * @dest: character
+ * @src: second char
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(*dest, src);
+}
+  

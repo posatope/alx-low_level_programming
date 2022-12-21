59e37a71f47ca948183d6aa9873d41adabdce273
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-21 01:36:08 -0700

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,6 +9,26 @@
  */
 void print_number(int n)
 {
-	print_number(n / 10);
-	putchar((n % 10) + '0');
+	if (n >= 0 && n <= 9)
+	{
+		_putchar(n  + '0');
+	}
+	else if (n > 9)
+	{
+		print_number(n / 10);
+		_putchar((n % 10) + '0');
+	}
+	else if (n < 0 && n >= -9)
+	{
+		_putchar('-');
+		n = -n;
+		_putchar(n  + '0');
+	}
+	else if (n < -9)
+	{
+		_putchar('-');
+		n = -n;
+		print_number(n/10);
+		_putchar((n % 10) + '0');
+	}
 }

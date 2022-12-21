a46d9d51b53cfbd891627d69598eaccae6891dfc
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-21 01:00:28 -0700

    Create 101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_number- check the code
+ * @n: integer to print
+ * Return: Always 0.
+ */
+void print_number(int n)
+{
+	putchar(n + '\0')

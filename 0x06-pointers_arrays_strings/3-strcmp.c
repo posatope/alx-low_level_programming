ee67f32324e18a79ac0029d4ce436c11acf18f1d
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-21 02:53:26 -0700

    Create 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strncpy - check the code
+ * @dest: char 1
+ * @src: char 2
+ * @n: int size
+ * Return: Always 0.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int x;
+	
+	x = strcmp(s1, s2); 
+	printf( "%d", x);
+}

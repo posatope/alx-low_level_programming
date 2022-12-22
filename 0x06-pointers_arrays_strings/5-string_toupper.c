d7f8ff8c7df8fbdb390a8e61951970cfee57879e
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-21 20:48:17 -0700

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,16 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h> 
 
 /**
  * _strncpy - check the code
  * @temp: char 1
  * Return: Always 0.
  */
-char *string_toupper(char *temp)
+char *string_toupper(char *)
 {
 	int i = 0;
-	char c;
 	char str[];
 	
 	while(str[i])

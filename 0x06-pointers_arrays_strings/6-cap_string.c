dec9ca87cb29fc0ef026f9e78cb644dd0884b404
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-22 19:23:08 -0700

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,21 +3,25 @@
 #include <string.h>
 #include <ctype.h> 
 #include <stdbool.h>
-
 /**
- * string_toupper - check the code
- * @str: char 1
- * Return: Always 0.
+ * cap_string - capitalizes all words of a string.
+ * @s: pointer to input string.
+ * Return: Returns pointer to capitalized string.
  */
-char *string_toupper(char *str)
+char *cap_string(char *s)
 {
-	long unsigned int a = 0;	
-	
-	while (a <= strlen(str))
+	int i, j;
+	char sep[] = " \t\n,;.!?\"(){}";
+
+	i = 1;
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] -= ('a' - 'A');
+	while (s[i] != '\0')
 	{
-		*str = toupper(str[a]);
-		putchar(toupper(str[a]));
-		a++;
+		for (j = 0; sep[j] != '\0'; j++)
+			if (s[i - 1] == sep[j] && (s[i] >= 'a' && s[i] <= 'z'))
+				s[i] -= ('a' - 'A');
+		i++;
 	}
-	return str;
+	return (s);
 }

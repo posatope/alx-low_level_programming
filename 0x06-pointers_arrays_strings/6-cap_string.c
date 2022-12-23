895f2e259a25f03b2518070ba05311d78e7e1a77
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-22 19:19:34 -0700

    Create 6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h> 
+#include <stdbool.h>
+
+/**
+ * string_toupper - check the code
+ * @str: char 1
+ * Return: Always 0.
+ */
+char *string_toupper(char *str)
+{
+	long unsigned int a = 0;	
+	
+	while (a <= strlen(str))
+	{
+		*str = toupper(str[a]);
+		putchar(toupper(str[a]));
+		a++;
+	}
+	return str;
+}

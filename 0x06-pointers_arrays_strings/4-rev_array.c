8082dbe3413c18cc0913914364f83e4277a9e9f5
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-29 06:08:00 -0700

    Create 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * reverse_array - check the code
+ * @a: char 1
+ * @n: char 2
+ * Return: Always 0.
+ */
+void reverse_array(int *a, int n)
+{
+}

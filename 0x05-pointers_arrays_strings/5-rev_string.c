f1ad00b891b4c6b577b20a4852a1710bdca857e8
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2022-12-30 00:36:41 -0700

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,10 +11,7 @@ void rev_string(char *s)
 	int c = strlen(s);
 	int a;
 
-	for (c = 0; s[c] != 0; c++)
-	{
-		_putchar('\n');
-	}
 	for (a = 0; a <= c; a++)
 		_putchar(s[c - a]);
+	_putchar('\n');
 }

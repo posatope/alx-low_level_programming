912441a242d641d5f72ee5f13095c03b0d55bcac
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-02 15:35:34 -0700

    Create 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strspn - prints buffer in hexa
+ * @s: the address of memory to print
+ * @accept: the size of the memory to print
+ *
+ * Return: s.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int n;
+
+    n = strspn(s, accept);
+    return (n);
+}

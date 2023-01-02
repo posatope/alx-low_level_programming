473736f2a3edf9518ef2098abcc4db1bd82bf625
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-02 14:39:24 -0700

    Create 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _memset - prints buffer in hexa
+ * @s: the address of memory to print
+ * @b: the size of the memory to print
+ * @n: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	memset(*s, b, n);
+}

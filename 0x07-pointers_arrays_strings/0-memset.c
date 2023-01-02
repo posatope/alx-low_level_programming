47d12b0d860a6031e0c7a4e6903f6036d934e303
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-02 15:15:28 -0700

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,16 +4,16 @@
 
 /**
  * _memset - prints buffer in hexa
- * @s: the address of memory to print
+ * @*s: the address of memory to print
  * @b: the size of the memory to print
  * @n: the size of the memory to print
  *
- * Return: Nothing.
+ * Return: s.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	void *d;
-	
+
 	d = &*s;
 	memset(d, b, n);
 	return (s);

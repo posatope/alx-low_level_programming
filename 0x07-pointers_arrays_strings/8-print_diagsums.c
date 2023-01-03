a5fede03e81a015ce07b6ba193a30ca7b6da705f
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-02 20:52:32 -0700

    Update 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,13 +12,13 @@
 void print_diagsums(int *a, int size)
 {
 	int i, j, sum = 0;
-	int mat[100][100];
+	int mat[i][j];
 
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
 		{
-			mat [100][100] = *a;
+			mat [i][j] = *a;
 			if(i == j)
 			{
 				sum = sum + mat[i][j];

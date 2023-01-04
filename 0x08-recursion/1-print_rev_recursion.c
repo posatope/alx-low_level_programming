a75d0f890805516a4ae2176d1b845ac2eb87e84b
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-04 14:45:44 -0700

    Create 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+#include <ctype.h>
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion - check the code
+ * @s: number to squareroot
+ * Return: Always 0.
+ */
+void _print_rev_recursion(char *s)
+{
+    if (*s)
+    {
+        _print_rev_recursion(s + 1);
+        printf("%c", *s); 
+  } 
+} 

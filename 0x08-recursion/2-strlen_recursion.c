a147df714e7afc4a4cf92ae7a46d8e22e21dcfb2
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-04 15:07:09 -0700

    Create 2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include <ctype.h>
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - check the code
+ * @s: number to squareroot
+ * Return: Always 0.
+ */
+int _strlen_recursion(char *s)
+{
+	int length = printf("%s", s);
+	return ("%d", length);
+}

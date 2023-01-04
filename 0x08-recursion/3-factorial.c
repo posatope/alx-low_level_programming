a6d9cb198aa986008f33fef8f1fc8c1574a5432f
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-04 08:57:07 -0700

    Update 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,14 +8,14 @@
  */
 int factorial(int n)
 {
-    if (n == 0)
-    {
-        return (1);
-    }
-    if (n < 0)
-    {
-        putchar('-');
-        return (1);
-    }
-    return (n * factorial(n - 1));
+	if (n == 0)
+	{
+		return (1);
+	}
+	if (n < 0)
+	{
+		putchar('-');
+		return (1);
+	}
+	return (n * factorial(n - 1));
 }

abd5e81f7acf63967ff32187a72ebf6066362708
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-06 06:05:26 -0700

    Create 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program prints its own name.
+ * @argc: argument count
+ * @argv: argument vectors
+ * Return: 0 on success
+ */
+int main(int argc, char **argv)
+{
+	argc--;
+	printf("%s\n", *argv++);
+	Return (0);
+}

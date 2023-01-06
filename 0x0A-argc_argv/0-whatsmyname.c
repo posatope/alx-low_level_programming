a85f0194ebc60af07c4825c7b0bbe95e00cc08c0
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-06 05:53:13 -0700

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+
 /**
  * main - check the code
- *
+ * @argc: count
+ * @argv: vector
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
 	argc--;
+
 	printf("%s", argv[0]);
 	return (0);
 }

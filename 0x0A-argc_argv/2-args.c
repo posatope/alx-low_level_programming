813f395f97389db1fdce10c88202ab34dd03ff34
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-06 06:02:36 -0700

    Create 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - program prints its own name.
+ * @argc: argument count
+ * @argv: argument vectors
+ * Return: 0 on success
+ */
+int main(int argc, char **argv)
+{
+	while(argc--)
+		printf("%s\n", *argv++);
+	exit(EXIT_SUCCESS);
+}

cb440a08bb56e82324b7aa1db37afb31c8205d5b
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-06 18:21:27 -0700

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,10 +12,14 @@ int main(int argc, char *argv[])
 	
 	argc--;
 	if (argc < 2)
+	{
 		printf("Error\n");
 		return (1);
+	}
 	else if (argc ==3)
+	{
 		d = (int) argv[1] * (int) argv[2];
 		printf("%d\n", d);
+	}
 	return (0);
 }

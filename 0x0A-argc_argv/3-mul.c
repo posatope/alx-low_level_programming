281c42b6ba405eee5fccb3e5d69ddc2094824b27
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-06 18:05:41 -0700

    Create 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - program prints its own name.
+ * @argc: argument count
+ * @argv: argument vectors
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	char c = "Error"
+	int d;
+	
+	arg--;
+	if (argc < 2)
+		printf("%s\n", c);
+		return (1);
+	else if (argc ==3)
+		d = argv[1] * argv[2];
+		printf("%d\n", d);
+	return (0);
+}

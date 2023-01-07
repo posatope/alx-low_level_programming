060d35c9d75622ddb3314d09aeb43e5eea40e63a
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-06 18:17:16 -0700

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,16 +6,16 @@
  * @argv: argument vectors
  * Return: 0 on success
  */
-int main(int argc, int *argv[])
+int main(int argc, char *argv[])
 {
 	int d;
 	
 	argc--;
 	if (argc < 2)
-		printf("Error%s\n");
+		printf("Error\n");
 		return (1);
 	else if (argc ==3)
-		d = argv[1] * argv[2];
+		d = (int) argv[1] * (int) argv[2];
 		printf("%d\n", d);
 	return (0);
 }

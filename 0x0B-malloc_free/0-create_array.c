3085aace8b6b7d0f598a743a6a696f851eb51e08
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-10 04:59:39 -0700

    Create 0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */

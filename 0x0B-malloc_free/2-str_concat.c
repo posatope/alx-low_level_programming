d9e993cdd752032dfe63eb2b84041fb997fd8827
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-10 19:49:04 -0700

    Create 2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - prints buffer in hexa
+ * @s1: the address of memory to print
+ * @s2: the address of memory to print
+ * Return: Nothing.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *s;
+	int i, j;
+
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	i++;
+  for (j = 0; s2[j] != '\0'; j++)
+		;
+	j++;
+	s = malloc((i + j) * sizeof(char));
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		s[i] = s1[i];
+		i++;
+	}
+  while (s2[j] != '\0')
+	{
+		s[i + j] = s2[j];
+		j++;
+	}
+	return (s);
+}

2178f9475d6a79cb10431a88a1afef0bc71eccf4
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-10 18:54:02 -0700

    Create 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - prints buffer in hexa
+ * @str: the address of memory to print
+ *
+ * Return: Nothing.
+ */
+char *_strdup(char *str)
+{
+	char *s;
+
+	s = malloc(sizeof(str));
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		while (i < size)
+		{
+			if (s == NULL)
+			{
+				return (NULL);
+			}
+			s = str;
+			free (s);
+		}
+	}
+	return (s);
+}

93869d6ca44d95a98f60abc91a53288bc30e6cbc
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-11 18:22:25 -0700

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,6 +20,12 @@ int **alloc_grid(int width, int height)
 	if (s == NULL)
 		return (NULL);
 	for (; i < height; i++)
+	{
+		s[i] = (int *)malloc(sizeof(int) * width);
+		if (s[i] == NULL)
+			return (NULL);
+	}
+	for (i = 0; i < height; i++)
 		for (; j < width; j++)
 			s[i][j] = 0;
 	return (s);

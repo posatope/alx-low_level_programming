4f3d3aa37b1fd91b92f459a5cd769f2c251c017d
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-12 18:30:40 -0700

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -37,7 +37,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		n = j;
 	}
-	s = malloc((i + n) * sizeof(char));
+	s = malloc((i + n + 1) * sizeof(char));
 	if (s == NULL)
 		return (NULL);
 	i = 0;
@@ -58,5 +58,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s[i + k] = s2[k];
 		k++;
 	}
+	s[i + k + 1] = '\0';
 	return (s);
 }

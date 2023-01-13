766d9d13c913412478648e4ec4d5a4b98257fbfb
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-12 19:47:45 -0700

    Create 3-array_range.c

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * array_range - A function that allocates memory using malloc.
+ * @min: An unsigned input integer
+ * @max: An unsigned input integer
+ * Return: Apointer to allocated memory or NULL if it fails
+ */
+int *array_range(int min, int max)
+{
+    int *s;
+    int i = 0, a;
+    
+    if (min > max)
+		return (NULL);
+	for (a = min; a <= max; a++)
+        i++;
+    printf("%d\n", i);
+    s = malloc(i);
+    if (s == NULL)
+        return (NULL);
+    i = 0;
+    for (a = min; a <= max; a++)
+    {
+        s[i] = a;
+        i++;
+    }
+    return (s);
+}

fa7518c81cd1238940a5ba1f16fa5eed8e08765d
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-12 18:19:19 -0700

    Create 1-string_nconcat.c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+/**
+ * findij - prints buffer in hexa
+ * @s1: the address of memory to print
+ * @i: the address of memory to print
+ * Return: Nothing.
+ */
+int findij(char *s1, int i)
+{
+	if (s1 == NULL)
+		i = 0;
+	else
+	{
+		for (i = 0; s1[i] != '\0'; i++)
+			;
+		i++;
+	}
+	return (i);
+}
+/**
+ * str_concat - prints buffer in hexa
+ * @s1: the address of memory to print
+ * @s2: the address of memory to print
+ * Return: Nothing.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *s;
+	int i = 0, j = 0, k = 0;
+
+	i = findij(s1, i);
+	j = findij(s2, j);
+	if (j == 0 || n >= j)
+	    n = j;
+	s = malloc((i + n) * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+	i = 0;
+	if (s1 == NULL)
+		i = 0;
+	else
+	while (s1[i] != '\0')
+	{
+		s[i] = s1[i];
+		i++;
+	}
+	k = 0;
+	if (s2 == NULL)
+		;
+	else
+	while (s2[j] != '\0' && k < n)
+	{
+		s[i + k] = s2[k];
+		k++;
+	}
+	return (s);
+}

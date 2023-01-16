e1e4c80b9fd70ad1d54f578505fc7a7206f3c0bb
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-16 12:28:31 -0800

    Update 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,19 +1,7 @@
-#include <stdio.h>
-
 #ifndef ABS(x)
 #define ABS(x) y
-#endif
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
 #if x < 0
 	y = x * -1;
 #else
 	y = x;
 #endif
-}

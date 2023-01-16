1aad43a3b3e8f3d4d1422c039d76b7ee7b565aee
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-16 12:02:27 -0800

    Update 0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,15 +1,3 @@
-#include <stdio.h>
-
-#define SIZE
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main()
-{
-	#ifndef SIZE
-	#define SIZE 1024
-	#endif
-}
+#ifndef SIZE
+#define SIZE 1024
+#endif

e0e302fd36ddc20ccd1affc87f0d15b36f537637
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-16 13:36:54 -0800

    Create 1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - structure for a basic dog
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ * Description: data structure for dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * @d- structure for a basic dog
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ * Return: Always 0.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return NULL;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}

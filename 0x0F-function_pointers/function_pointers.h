d094edcd50b5692656a395bfc3cf5b981773b7bc
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-18 15:13:12 -0800

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,10 @@
-void print_name(char *name, void (*f)(char *));
-int int_index(int *array, int size, int (*cmp)(int));
-void array_iterator(int *array, size_t size, void (*action)(int));
+#ifndef ASSIGN_0x0E
+#define ASSIGN_0x0E
+
 #include <stddef.h>
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+int _putchar(char c);
+
+#endif

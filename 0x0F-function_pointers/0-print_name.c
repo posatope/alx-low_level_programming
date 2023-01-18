d248250af990ec3564993bd499f7284abddc92e4
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-18 14:14:18 -0800

    Create 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - prints a name as is
+ f - prints a name as is
+ * @name: name of the person
+ *
+ * Return: Nothing.
+ */
+ void print_name(char *name, void (*f)(char *))
+ {
+     f(name);
+ }

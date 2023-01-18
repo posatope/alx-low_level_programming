31407bdc3e3dae5d4ef999c8f0501afab7dfcf9a
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-18 15:04:36 -0800

    Create 1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * array_iterator - prints a name as is
+ * @array: prints a name as is
+ * @size: name of the person
+ * @action: prints a name as is
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i = 0;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+ }

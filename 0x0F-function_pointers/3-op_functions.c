dcdd15c04864d7962da830fc76e4f7c498efb81a
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-19 12:55:32 -0800

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -28,7 +28,7 @@ int op_sub(int a, int b)
  * @b: The operator
  * Return: Nothing.
  */
-int op_mul(int a, int b);
+int op_mul(int a, int b)
 {
 	return ((a) * (b));
 }
@@ -38,7 +38,7 @@ int op_mul(int a, int b);
  * @b: The operator
  * Return: Nothing.
  */
-int op_div(int a, int b);
+int op_div(int a, int b)
 {
 	if (b == 0)
 	{
@@ -53,7 +53,7 @@ int op_div(int a, int b);
  * @b: The operator
  * Return: Nothing.
  */
-int op_mod(int a, int b);
+int op_mod(int a, int b)
 {
 	if (b == 0)
 	{

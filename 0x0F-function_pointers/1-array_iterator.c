7a829ab648643a4b93921193cd802086a6fe8beb
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-19 11:18:17 -0800

    Update 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,7 +13,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	unsigned long int i = 0;
 
 	if (array == NULL || action == NULL)
-		exit;
+		return;
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);

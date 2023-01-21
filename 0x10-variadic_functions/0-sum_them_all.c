e9a5b89e8f1cc56ab16542d75a3af3a3169781c4
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-21 16:48:06 -0700

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -16,9 +16,8 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(ap, n);
 	if (n == 0)
 		return (0);
-	else
-		for (i = 0; i < n; i++)
-			sum += va_arg(ap, int);
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
 	va_end(ap);
 	return (sum);
 }

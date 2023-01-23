e33c70990c25b4991a509065179384b3f4564d84
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-23 16:11:06 -0700

    Update t_char_functions.c

--- printf/t_char_functions.c
+++ printf/t_char_functions.c
@@ -1,26 +1,55 @@
-char t_char(va_list ap);
+char t_char(va_list ap)
+{
+}
+/**
+  * t_string - passes a string through
+  * @ap: string to pass through
+  * Return: unmodified string
+  */
+char *t_string(va_list ap)
+{
+	char *string;
 
-char t_string(va_list ap); 
-char t_percent(va_list ap);
-char t_int(va_list ap);
-char t_unsigned(va_list ap);
-char t_octal(va_list ap);
+	string = va_arg(ap, char *);
+	if (string == NULL)
+		return ("(null)");
+	return (string);
+}
+/**
+  * t_percent - passes a % sign through
+  * @ap: string to pass throguh
+  * Return: percent sign as a character string
+  */
+char *t_percent()
+{
+    char *s;
+    
+    s = "%";
+	return (s);
+}
 /**
  * t_reverse - reverses a string
  * @ap: string to reverse
  * Return: pointer to string
  */
-char t_reverse(va_list ap)
+char *t_reverse(va_list ap)
 {
-	
-	int c;
-	char s
+	unsigned int i = 0, n;
+	char *string, s;
 
-	for (c = 0; ap[c] != 0; c++)
+	string = va_arg(ap, char*);
+	n = strlen(string);
+	if (string == NULL)
+			return ("(null)");
+	for (; i < n; i++)
 	{
 	}
-	for (c = c - 1; c >= 0; c--)
-		s = (ap[c]);
-	return (s);
+	for (i = i - 1; i >= 0; i--)
+	    s = string[i];
+	    
+	string[i] = s;
+	return (string);
 }
-
+char t_int(va_list ap);
+char t_unsigned(va_list ap);
+char t_octal(va_list ap);

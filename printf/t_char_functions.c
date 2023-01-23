7e73b0e2e9b12238ae2d4e72c89b1f3847933fba
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-23 13:48:13 -0700

    Update t_char_functions.c

--- printf/t_char_functions.c
+++ printf/t_char_functions.c
@@ -1,8 +1,9 @@
-char t_char();
-char t_string(); 
-char t_percent();
-char t_int();
-char t_unsigned();
-char t_octal();
-char t_reverse();
-char t_rot13();
+char t_char(va_list ap);
+
+char t_string(va_list ap); 
+char t_percent(va_list ap);
+char t_int(va_list ap);
+char t_unsigned(va_list ap);
+char t_octal(va_list ap);
+char t_reverse(va_list ap);
+char t_rot13(va_list ap);

cc5c4835fe5854dd9ef3796b4e6690249fa3e700
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-23 14:09:19 -0700

    Update t_char_functions.c

--- printf/t_char_functions.c
+++ printf/t_char_functions.c
@@ -5,5 +5,22 @@ char t_percent(va_list ap);
 char t_int(va_list ap);
 char t_unsigned(va_list ap);
 char t_octal(va_list ap);
-char t_reverse(va_list ap);
-char t_rot13(va_list ap);
+/**
+ * t_reverse - reverses a string
+ * @ap: string to reverse
+ * Return: pointer to string
+ */
+char t_reverse(va_list ap)
+{
+	
+	int c;
+	char s
+
+	for (c = 0; ap[c] != 0; c++)
+	{
+	}
+	for (c = c - 1; c >= 0; c--)
+		s = (ap[c]);
+	return (s);
+}
+

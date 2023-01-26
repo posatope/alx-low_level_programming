d64d6f029e2b07eec266fe2069187f639b6aec83
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-26 10:56:50 -0700

    Create 0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - print list
+ * @h: list to print
+ * Return: size of h
+ */
+size_t print_list(const list_t *h)
+{
+    int i = 0;
+
+    while (h)
+    {
+        printf("[%u] ", h->len);
+        if (h->str == NULL)
+        {
+            printf("[0] (nil)\n");
+            return (1);
+        }
+        else
+            printf("%s\n", h->str);
+        h = h->next;
+        i++;
+    }
+    return (i);
+}

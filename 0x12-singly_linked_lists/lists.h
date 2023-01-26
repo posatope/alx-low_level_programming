b53bdce11a4050ab24b0ebc8a591148e5ab430a3
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-26 10:35:35 -0700

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,21 @@
-#ifdef LIST_H
+#ifndef LIST_H
 #define LIST_H
+#include <stdlib.h>
+
+/**
+ * struct node - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct node {
+	char *str;
+	int len;
+	struct node *next;
+}list_t;
+
 size_t print_list(const list_t *h);
 
 #endif

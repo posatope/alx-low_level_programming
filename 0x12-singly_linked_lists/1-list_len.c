c1cf088bdd3ebf8ac9a5462cae34a45fbf4633d1
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-26 11:12:45 -0700

    Create 1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - print list
+ * @h: list to print
+ * Return: size of h
+ */
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			return (1);
+		}
+		h = h->next;
+		i++;
+	}
+	return (i);
+}

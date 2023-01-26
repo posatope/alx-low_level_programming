d70ddc2998e4a02f16cc37c46eb920c3908c8287
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-26 12:37:57 -0700

    Update 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,9 @@
 mov       rax, 1                  ;
-system call for write          mov       rdi, 1                  ;
-file handle 1 is stdout          mov       rsi, message            ;
-address of string to output          mov       rdx, 13                 ;
-number of bytes          syscall                           ;
-invoke operating system to do the write          mov       rax, 60                 ;
-system call for exit          xor       rdi, rdi                ;
-exit code 0          syscall                           ;
-invoke operating system to exit          section   .datamessage:  db        "Hello, World", 10      ;
+mov       rdi, 1                  ;
+mov       rsi, message            ;
+mov       rdx, 13                 ;
+syscall                           ;
+mov       rax, 60                 ;
+xor       rdi, rdi                ;
+syscall                           ;
+db        "Hello, World", 10      ;

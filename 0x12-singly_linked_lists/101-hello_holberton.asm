2cf31dc49528a947e7c451a9a1015cfd75b0bff5
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-26 13:26:27 -0700

    Update 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,18 @@
-mov       rax, 1                  ;
-mov       rdi, 1                  ;
-mov       rsi, message            ;
-mov       rdx, 13                 ;
-syscall                           ;
-mov       rax, 60                 ;
-xor       rdi, rdi                ;
-syscall                           ;
-db        "Hello, World", 10      ;
+global _start
+
+section .text
+
+_start:
+  mov rax, 1        ; write(
+  mov rdi, 1        ;   STDOUT_FILENO,
+  mov rsi, msg      ;   "Hello, Holberton\n",
+  mov rdx, msglen   ;   sizeof("Hello, Holberton\n")
+  syscall           ; );
+
+  mov rax, 60       ; exit(
+  mov rdi, 0        ;   EXIT_SUCCESS
+  syscall           ; );
+
+section .rodata
+  msg: db "Hello, Holberton", 10
+  msglen: equ $ - msg

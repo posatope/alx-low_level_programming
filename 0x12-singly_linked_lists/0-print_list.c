e877fdb856abae47ed748817dcdbdb2bfe964f9b
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-27 08:51:44 -0700

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,13 +12,13 @@ size_t print_list(const list_t *h)
 	int i = 0;
 	while (h)
 	{
-		printf("[%u] ", h->len);
 		if (h->str == NULL)
 		{
 			printf("[0] (nil)\n");
 		}
 		else
 		{
+			printf("[%u] ", h->len);
 			printf("%s\n", h->str);
 		}
 		h = h->next;

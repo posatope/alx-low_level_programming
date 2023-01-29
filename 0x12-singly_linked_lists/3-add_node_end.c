149bfaf909b3da9f75a0dfdb13c11652c5fc25c1
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-29 16:35:15 -0700

    Update 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -22,12 +22,12 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node->len = i;
 	new_node->str = strdup(str);
 	new_node->next = NULL;
-	if(*head == NULL)
+	if (*head == NULL)
 		*head = new_node;
 	else
 	{
 		list_t *last_node = *head;
-		while(last_node->next != NULL)
+		while (last_node->next != NULL)
 		{
 			last_node = last_node->next;
 		}

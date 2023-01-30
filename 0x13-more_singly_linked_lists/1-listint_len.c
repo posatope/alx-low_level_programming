cda779619faf677182a179f86c00773399fc0119
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-30 10:07:58 -0700

    Create 1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * list_len - print list
+ * @h: list to print
+ * Return: size of h
+ */
+size_t listint_len(const listint_t *h)
+{
+	int i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}

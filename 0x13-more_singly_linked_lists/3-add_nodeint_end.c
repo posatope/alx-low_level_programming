001c8cbeab880b6d1b7764e9fdb9869b46c33b85
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-30 09:59:52 -0700

    Create 3-add_nodeint_end.c

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * *add_nodeint_end - print list
+ * @*head: list to print
+ * @n: new string
+ * Return: size of h
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new_node;
+	int i = 0;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	while (str[i])
+		i++;
+
+	new_node->len = i;
+	new_node->n = n;
+	new_node->*next = NULL;
+	if (**head == NULL)
+		**head = new_node;
+	else
+	{
+		listint_t *last_node = **head;
+
+		while (last_node->*next != NULL)
+		{
+			last_node = last_node->*next;
+		}
+		last_node->*next = new_node;
+	}
+	return (**head);
+}

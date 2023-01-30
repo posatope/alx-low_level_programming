9b62357adde8d7b646e2a6dcd77e4e988a5a7fb8
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-30 10:13:11 -0700

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -13,14 +13,7 @@ size_t print_listint(const listint_t *h)
 
 	while (h)
 	{
-		if (h->n == NULL)
-		{
-			printf("[0] (nil)\n");
-		}
-		else
-		{
-			printf("%d\n", h->n);
-		}
+		printf("%d\n", h->n);
 		h = h->*next;
 		i++;
 	}

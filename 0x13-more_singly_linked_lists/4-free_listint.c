9b58a825908861bab0149bc108548188d922a54a
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-29 23:15:37 -0700

    Create 4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - A function that frees all elements in a linked list
+ * @head: pointer to head element of list
+ * Return: Nothing
+ */
+void free_listint(listint_t *head)
+{
+	int i = 0;
+
+	while (head)
+	{
+		free(head->str);
+		free(head);
+		head = head->next;
+		i++;
+	}
+}

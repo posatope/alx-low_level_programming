725e0f4363b1be3587cb09091d75ee48dfc12956
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-31 09:50:40 -0700

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,19 +1,24 @@
 #include "lists.h"
 
-/**
- * free_list - A function that frees all elements in a linked list
+ * free_listint2 - A function that frees all elements in a linked list
  * @head: pointer to head element of list
  * Return: Nothing
  */
 void free_listint2(listint_t **head)
 {
 	int i = 0;
-
+	listint_t *x, *y;
+	if (*head == NULL)
+	    return;
 	while (*head)
 	{
-		free(*head->str);
+		x = *head;
+		if (x == NULL)
+			return;
+
+		y = x->next;
+		free_listint2(x);
 		free(*head);
-		*head = *head->next;
 		i++;
 	}
 }

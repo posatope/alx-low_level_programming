6829e211f7c1529fa250b47a889566522b897a99
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-02-01 10:27:47 -0700

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,11 +13,12 @@ void free_listint2(listint_t **head)
 
 	if (head == NULL)
 		return;
-	while (*head != NULL)
+	while (*head)
 	{
 		x = *head;
 		(*head) = (*head)->next;
 		free(x);
 	}
+	*head = NULL;
 	free(head);
 }

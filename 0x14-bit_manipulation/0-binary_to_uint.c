6dfa11ffc9940397f418314602b876edafe2ad4b
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-02-03 15:05:38 -0700

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -29,6 +29,8 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int i;
 	unsigned int m = 0;
 
+	if (b == NULL)
+		return (0);
 	for (i = 0; i < strlen(b); i++)
 	{
 		if (b[i] != '0' && b[i] != '1')

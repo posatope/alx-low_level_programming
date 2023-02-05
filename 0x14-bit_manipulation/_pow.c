4d4b9499d3fd433b7d74f4bff635050a8b95cb99
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-02-04 23:03:32 -0700

    Create _pow.c

---
+++ 0x14-bit_manipulation/_pow.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include "main.h"
+#include <string.h>
+/**
+ * _pow - function that powers
+ * @a: base
+ * @d: number of times
+ * Return: the converted number, or 0
+ */
+int _pow(int a, int d)
+{
+	int i = 0, pow = 1;
+
+	if (d == 0)
+		return (1);
+	for (; i < d; i++)
+		pow = pow * a;
+	return (pow);
+}

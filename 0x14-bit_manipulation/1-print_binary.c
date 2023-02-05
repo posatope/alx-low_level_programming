e6f9b06626ee65d728835381b12dd47ed9e0c612
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-02-04 22:41:42 -0700

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,6 +3,23 @@
 #include <math.h>
 #include "main.h"
 /**
+ * _pow - function that powers
+ * @a: base
+ * @d: number of times
+ * Return: the converted number, or 0
+ */
+int _pow(int a, int d)
+{
+	int i = 0;
+	int pow = 1;
+
+	if (d == 0)
+		return (1);
+	for (; i < d; i++)
+		pow = pow * a;
+	return (pow);
+}
+/**
  * print_binary - convert to binary
  * @n: number to be converted
  * Return: Always 0.
@@ -19,45 +36,45 @@ void print_binary(int n)
     else 
     {
         for (; i < n; i++)
-            if (pow(2, i) > n)
+            if (_pow(2, i) > n)
             {
                 break;
             }
         d = i - 1;
         printf("%d", 1);
-        rem = n - (pow(2, d));
+        rem = n - (_pow(2, d));
         if (rem == 0)
         {
             for (; j < d - 1; j++)
-                putchar('0');
+                _putchar('0');
         }
         if (rem == 1)
         {
             for (; j < d - 2; j++)
-                putchar('0');
-            putchar('0');
-            putchar(1 + '\0');
+                _putchar('0');
+            _putchar('0');
+            _putchar(1 + '\0');
         }
         if (rem == 2)
         {
             for (; j < d - 3; j++)
-                putchar('0');
-            putchar(1 + '\0');
-            putchar('0');
+                _putchar('0');
+            _putchar(1 + '\0');
+            _putchar('0');
         }
         if (rem == 3)
         {
             for (; j < d - 3; j++)
-                putchar('0');
-            putchar(1 + '\0');
-            putchar(1 + '\0');
+                _putchar('0');
+            _putchar(1 + '\0');
+            _putchar(1 + '\0');
         }
         else
         {
             
             for (i = 0; i < d; i++)
-                if (pow(2, (d - i)) < rem)
-                    putchar('0');
+                if (_pow(2, (d - i)) < rem)
+                    _putchar('0');
                 else if (pow(2, (d - i)) >= rem)
                 {
                     print_binary(rem);

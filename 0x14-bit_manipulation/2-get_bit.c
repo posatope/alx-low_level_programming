d03fef4c95779129088e8d6ba0f1fd8069983507
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-02-05 01:24:45 -0700

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -14,7 +14,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int num = log2(n) +1;
 
-	if ((index >= num) || n == NULL)
+	if ((index >= num) || &n == NULL)
 		return(-1);
 	return(((n >> index) & 1));
 }

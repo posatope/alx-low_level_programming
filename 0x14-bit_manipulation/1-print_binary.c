98ebed025ebfc9c770ad1f7deb453a299e8b6e5a
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-02-04 23:34:08 -0700

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -30,7 +30,7 @@ void print_binary(unsigned long int n)
     unsigned long int i = 0, f, j = 0, rem;
 
     if (n == 0 || n == 1)
-        _putchar(n + '\0');
+        putchar(n + '\0');
     else 
     {
         for (; i < n; i++)
@@ -39,40 +39,40 @@ void print_binary(unsigned long int n)
                 break;
             }
         f = i - 1;
-        _putchar(1 + '\0');
+        putchar(1 + '\0');
         rem = n - (_pow(2, f));
         if (rem == 0)
         {
             for (; j < f - 1; j++)
-                _putchar('0');
+                putchar('0');
         }
         if (rem == 1)
         {
             for (; j < f - 2; j++)
-                _putchar('0');
-            _putchar('0');
-            _putchar(1 + '\0');
+                putchar('0');
+            putchar('0');
+            putchar(1 + '\0');
         }
         if (rem == 2)
         {
             for (; j < f - 3; j++)
-                _putchar('0');
-            _putchar(1 + '\0');
-            _putchar('0');
+                putchar('0');
+            putchar(1 + '\0');
+            putchar('0');
         }
         if (rem == 3)
         {
             for (; j < f - 3; j++)
-                _putchar('0');
-            _putchar(1 + '\0');
-            _putchar(1 + '\0');
+                putchar('0');
+            putchar(1 + '\0');
+            putchar(1 + '\0');
         }
         else
         {
             
             for (i = 0; i < f; i++)
                 if (_pow(2, (f - i)) < rem)
-                    _putchar('0');
+                    putchar('0');
                 else if (_pow(2, (f - i)) >= rem)
                 {
                     print_binary(rem);

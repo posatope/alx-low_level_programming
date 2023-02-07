335d0b4de36fe18aa849b228f4e205658816d676
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-02-06 19:54:17 -0700

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,9 +18,9 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, i);
 	if (text_content != NULL)
 		i = strlen(text_content);
+	w = write(o, text_content, i);
 	if (o == -1 || w == -1)
 		return (-1);
 	close(o);
